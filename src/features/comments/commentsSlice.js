d11fc292aef32923e0bd2d471089fb10f5c48fd7
James <jkeezer@pm.me>
2023-05-01 17:12:36 -0400

    Initial commit

---
+++ src/features/comments/commentsSlice.js
@@ -0,0 +1,47 @@
+// Import createAsyncThunk and createSlice here.
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+// Create loadCommentsForArticleId here.
+export const loadCommentsForArticleId = createAsyncThunk(
+  'comments/loadCommentsForArticleId',
+  async (articleId) => {
+    const response = await fetch(`api/articles/${articleId}/comments`);
+    const json = await response.json();
+    return json;
+  }
+)
+
+// Create postCommentForArticleId here.
+
+export const commentsSlice = createSlice({
+  name: 'comments',
+  initialState: {
+    // Add initial state properties here.
+    byArticleId: {},
+    isLoadingComments: false,
+    failedToLoadComments: false,
+  },
+  // Add extraReducers here.
+  extraReducers: (builder) => {
+    builder
+    .addCase(loadCommentsForArticleId.pending, (state) => {
+      state.isLoadingComments = true;
+      state.failedToLoadComments = false;
+    })
+    .addCase(loadCommentsForArticleId.fulfilled, (state, action) => {
+      state.isLoadingComments = false;
+      state.failedToLoadComments = false;
+      state.byArticleId = action.payload;
+    })
+    .addCase(loadCommentsForArticleId.rejected, (state) => {
+      state.isLoadingComments = false;
+      state.failedToLoadComments = true;
+    })
+  }
+});
+
+export const selectComments = (state) => state.comments.byArticleId;
+export const isLoadingComments = (state) => state.comments.isLoadingComments;
+export const createCommentIsPending = (state) => state.comments.createCommentIsPending;
+
+export default commentsSlice.reducer;
